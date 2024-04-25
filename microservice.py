ecb5a8c0dc139b8ab4eac79e812ec77da5744531
Tanmayee006 <149505841+Tanmayee006@users.noreply.github.com>
2024-04-25 11:09:34 +0530

    Update microservice.py

--- microservice.py
+++ microservice.py
@@ -1,11 +1,8 @@
-from flask import Flask, jsonify
- 
-app = Flask(__name__)
- 
-@app.route("/hello", methods=['GET'])
-def hello_microservice():
-    message = {"message": "Hello from the microservice! This is GeeksForGeeks"}
-    return jsonify(message)
- 
-if __name__ == "__main__":
-    app.run(port=8000)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
