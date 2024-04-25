7f0b087c004d1128fa84c91fa95226b1f802f43b
Tanmayee006 <149505841+Tanmayee006@users.noreply.github.com>
2024-04-25 11:01:29 +0530

    Update microservice.py

--- microservice.py
+++ microservice.py
@@ -1,8 +1,11 @@
-# from flask import Flask, jsonify
-from flask import Flask
+from flask import Flask, jsonify
+ 
 app = Flask(__name__)
-
-@app.route("/")
+ 
+@app.route("/hello", methods=['GET'])
 def hello_microservice():
-	# message = {"message": "Hello from the microservice! This is Tanmayee"}
-	return "<p>Hello World</p>"
+    message = {"message": "Hello from the microservice! This is GeeksForGeeks"}
+    return jsonify(message)
+ 
+if __name__ == "__main__":
+    app.run(port=8000)
